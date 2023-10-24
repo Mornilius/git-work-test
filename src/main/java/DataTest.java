44661b65f204c17e68d1c14be29fbf73d92876b5
Mornilius <121147303+Mornilius@users.noreply.github.com>
2023-10-24 17:50:21 +0300

    add func click, inputText

--- src/main/java/DataTest.java
+++ src/main/java/DataTest.java
@@ -1,19 +1,22 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DataTest {
-    static final String USEREMAIL = "protei@test.ru";
+    static final String USEREMAIL = "test@protei.ru";
     static final String USERPASSWORD = "test";
-    static final String EMAILEXPATH = "input[@id='loginEmail']";
-    static final String PASSWORDEXPATH = "input[@id='loginPassword']";
+    static final String EMAILEXPATH = "//input[@id='loginEmail']";
+    static final String PASSWORDEXPATH = "//input[@id='loginPassword']";
     static final String Website = "C:\\tools\\qa-test.html";
     static WebElement element;
 
-
-    public void click(WebDriver driver, String locator){
+    public void clickOnTop(FirefoxDriver driver, String locator){
         element = driver.findElement(By.xpath(locator));
         element.click();
+    }
+    public void sendText(String text){
+        element.sendKeys(text);
     }
 }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,16 +5,22 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import javax.xml.crypto.Data;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
-        WebDriver driver = new FirefoxDriver();
+        FirefoxDriver driver = new FirefoxDriver();
         DataTest datatest = new DataTest();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        driver.get(DataTest.Website);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
+        driver.get(DataTest.Website);
+        datatest.clickOnTop(driver, DataTest.EMAILEXPATH);
+        datatest.sendText(DataTest.USEREMAIL);
+        datatest.clickOnTop(driver, DataTest.PASSWORDEXPATH);
+        datatest.sendText(DataTest.USERPASSWORD);
+        datatest.clickOnTop(driver, "//button[@id='authButton']");
 
 
 
