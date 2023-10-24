04dbe3db371dded571de5a5ae682be7abb8a2b04
Mornilius <121147303+Mornilius@users.noreply.github.com>
2023-10-24 19:05:54 +0300

    adding test framework

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,11 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import javax.xml.crypto.Data;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         FirefoxDriver driver = new FirefoxDriver();
         DataTest datatest = new DataTest();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
         driver.get(DataTest.Website);
         datatest.clickOnTop(driver, DataTest.EMAILEXPATH);
         datatest.sendText(DataTest.USEREMAIL);
