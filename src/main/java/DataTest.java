0f1846927ae66dbbf401f97d757b6f246252000e
Mornilius <121147303+Mornilius@users.noreply.github.com>
2023-10-24 16:12:27 +0300

    add default userdata

--- src/main/java/DataTest.java
+++ src/main/java/DataTest.java
@@ -1,3 +1,6 @@
 public class DataTest {
-
+    static final String USEREMAIL = "protei@test.ru";
+    static final String USERPASSWORD = "test";
+    static final String EMAILEXPATH = "input[@id='loginEmail']";
+    static final String PASSWORDEXPATH = "input[@id='loginPassword']";
 }
