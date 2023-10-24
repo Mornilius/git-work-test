a418d4da42bc142b89ecdf3a2138b9ded62878b3
Mornilius <121147303+Mornilius@users.noreply.github.com>
2023-10-24 16:05:19 +0300

    start test

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("C:\\tools\\qa-test.html");
+    }
+}
