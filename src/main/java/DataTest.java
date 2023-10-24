de2144c9311539d163d39e8449ea9c08e1b1e6ad
Mornilius <121147303+Mornilius@users.noreply.github.com>
2023-10-24 17:25:05 +0300

    add func click

--- src/main/java/DataTest.java
+++ src/main/java/DataTest.java
@@ -1,6 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class DataTest {
     static final String USEREMAIL = "protei@test.ru";
     static final String USERPASSWORD = "test";
     static final String EMAILEXPATH = "input[@id='loginEmail']";
     static final String PASSWORDEXPATH = "input[@id='loginPassword']";
+    static final String Website = "C:\\tools\\qa-test.html";
+    static WebElement element;
+
+
+    public void click(WebDriver driver, String locator){
+        element = driver.findElement(By.xpath(locator));
+        element.click();
+    }
 }
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,23 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         WebDriver driver = new FirefoxDriver();
-        driver.get("C:\\tools\\qa-test.html");
-        
+        DataTest datatest = new DataTest();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+        driver.get(DataTest.Website);
+
+
+
+
+
     }
 }
